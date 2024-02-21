134e08027072615adcf72adf57aaa7c0af9323cc
Octavio Luperón <armando2445@outlook.com>
2024-02-21 15:02:52 -0400

    made a basic reactive form

--- src/app/reactive/pages/dynamicPage/dynamicPage.component.ts
+++ src/app/reactive/pages/dynamicPage/dynamicPage.component.ts
@@ -1,12 +1,8 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-page',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './dynamicPage.component.html',
   styles: ``,
 })
--- src/app/reactive/pages/switchesPage/switchesPage.component.ts
+++ src/app/reactive/pages/switchesPage/switchesPage.component.ts
@@ -1,12 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches-page',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './switchesPage.component.html',
   styles: ``,
 })
